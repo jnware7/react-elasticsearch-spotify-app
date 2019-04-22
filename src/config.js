84b361bbdc9efb78f8febdc7916b69790e260895
jnware7 <jnware7@gmail.com>
2019-04-22 09:57:32 -0700

    see this fixes error on heroku

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = process.env.REACT_APP_CLIENT_ID;
+// process.env.REACT_APP_CLIENT_ID;
+export const clientId = "e204bacf8e4f4c3496f9a2ddd7664a7e"
 // export const redirectUri = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_REDIRECT : "http://localhost:3000/callback/";
-export const redirectUri = process.env.REACT_APP_REDIRECT;
+// process.env.REACT_APP_REDIRECT;
+export const redirectUri = "http://localhost:3000/callback"
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
