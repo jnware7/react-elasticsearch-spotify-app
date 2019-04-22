e70322ac13604e6c9c62b17de6e0f225c3261840
jnware7 <jnware7@gmail.com>
2019-04-22 10:21:36 -0700

    got 3000 to work now trying actual redirect https://shielded-sands-75814.herokuapp.com/

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "e204bacf8e4f4c3496f9a2ddd7664a7e"
 // export const redirectUri = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_REDIRECT : "http://localhost:3000/callback/";
 // process.env.REACT_APP_REDIRECT;
-export const redirectUri = "http://localhost:3000"
+export const redirectUri = "https://shielded-sands-75814.herokuapp.com"
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
