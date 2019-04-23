c9d6a691fc59917de07205997a650839f1e783a2
jnware7 <jnware7@gmail.com>
2019-04-22 18:13:11 -0700

    get url external

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ handelSubmit(e){
   const options = {};
 
   client.search(this.state.searchString, options)
-    .then(resultList => console.log(resultList, "elasticsearch"))
+    .then(resultList => console.log(resultList[0].external_urls.raw, "elasticsearch"))
     .catch(error => console.log(error))
 
 }
