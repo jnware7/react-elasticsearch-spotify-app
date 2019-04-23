6d85adc0eea6b3de0c64e64f362cbabb026f7401
jnware7 <jnware7@gmail.com>
2019-04-23 08:33:07 -0700

    trying to get data from elastic response

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ handelSubmit(e){
   const options = {};
 
   client.search(this.state.searchString, options)
-    .then(resultList => console.log(JSON.stringify(resultList), "elasticsearch"))
+    .then(resultList => console.log(JSON.stringify(resultList[0].artists.raw[0]), "elasticsearch"))
     .catch(error => console.log(error))
 
 }
