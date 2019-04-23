a70527c2f8cbe79ac8b50dd05deb6e193db93f45
jnware7 <jnware7@gmail.com>
2019-04-23 09:56:20 -0700

    tried json.parse now

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ handelSubmit(e){
   const options = {};
 
   client.search(this.state.searchString, options)
-    .then(resultList => console.log(resultList, "elasticsearch"))
+    .then(resultList => console.log(JSON.parse(resultList[0].artists), "elasticsearch"))
     .catch(error => console.log(error))
 
 }
