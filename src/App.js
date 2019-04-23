78c1c406e34277b58352bdcf3cbdbda65c7b0b05
jnware7 <jnware7@gmail.com>
2019-04-22 18:03:08 -0700

    need to look at full json return

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ handelSubmit(e){
   const options = {};
 
   client.search(this.state.searchString, options)
-    .then(resultList => console.log(JSON.stringify(resultList.external_urls), "elasticsearch"))
+    .then(resultList => console.log(resultList, "elasticsearch"))
     .catch(error => console.log(error))
 
 }
