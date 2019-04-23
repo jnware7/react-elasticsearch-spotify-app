bdbd34fadce92a59e6475972f33d1425000eb54d
jnware7 <jnware7@gmail.com>
2019-04-22 18:44:43 -0700

    data issue

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ handelSubmit(e){
   const options = {};
 
   client.search(this.state.searchString, options)
-    .then(resultList => console.log(resultList[0], "elasticsearch"))
+    .then(resultList => console.log(resultList, "elasticsearch"))
     .catch(error => console.log(error))
 
 }
