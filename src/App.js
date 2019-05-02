64f4bb0ce865c4205b2a88c5a108f7a7bea5ce20
jnware7 <jnware7@gmail.com>
2019-05-02 11:45:00 -0700

    trying to print response in pure json

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
             },
             success: (data) => {
                 console.log("GET to request to https://api.spotify.com/v1/browse/new-releases was succesful.");
-                console.dir(data,"spotify data");
+                console.dir(JSON.parse(data),"spotify data");
                 const arrayOfAlbums = data.albums.items;
                 let arrayToState = [];
                 arrayOfAlbums.forEach(album => {
